61ba66e176ed13fff4bd5a333cfbb335c7c00113
Stephen <stephen4951@naver.com>
2020-08-31 21:42:47 +0900

    gitignore 등록

--- src/main/java/com/stephen/springboot/service/posts/PostsService.java
+++ src/main/java/com/stephen/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.stephen.springboot.service.posts;
 
 import com.stephen.springboot.domain.posts.Posts;
 import com.stephen.springboot.domain.posts.PostsRepository;
+import com.stephen.springboot.web.dto.PostsListResponseDto;
 import com.stephen.springboot.web.dto.PostsResponseDto;
 import com.stephen.springboot.web.dto.PostsSaveRequestDto;
 import com.stephen.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,6 +39,13 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
     }
 
 }
